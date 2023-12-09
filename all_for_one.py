865df5fef73b1394422e6cdf6812dcb94d6fe2ca
Aaryl Saldanha <estorossa12seven@gmail.com>
2023-12-09 14:52:55 -0500

    change hyparams

--- all_for_one.py
+++ all_for_one.py
@@ -25,6 +25,7 @@
 import pygame
 import multiprocessing
 from game import game
+from hyparams import num_processes
 import os
 
 pygame.init()
@@ -33,11 +34,9 @@ def instances(num):
     posi = [(10,50), (10,500), (400,50), (400,500), (800,50), (800,500), (1180,50), (1180,500)]
     x, y = posi[num]
     os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (x,y)
-    print(num,x,y)
     game()
 
 if __name__=="__main__":
-    num_processes = 8  # Define the number of processes
     processes = []
     
     for i in range(num_processes):
--- hyparams.py
+++ hyparams.py
@@ -3,11 +3,12 @@ cell_size = 4                      # cell size
 cell_number = 80                    # no of cells in width
 offset = 3                          # cell padding around simulation
 number_of_particles = 20            # no of particles in simulation
-screen_update_timer = 100          # time between game updates in millisec
+screen_update_timer = 1500          # time between game updates in millisec
 
 alive_color = (0,0,255)             # alive state color
 dead_color = (0,0,0)                # dead state color
 
+num_processes = 8
 
 sim_offset = offset * cell_size
 #   snake hyper paraemters
